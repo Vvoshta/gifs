c636aa3de1fbe340f5ac21a4e672888c2967541e
Valeriia <vvoshta@gmail.com>
2024-04-16 17:52:04 +0300

    init

---
+++ src/components/TrendsPage/TrendsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MainBreadcrumbs } from '../Breadcrumbs/Breadcrumbs';
+
+const TrendsPage: React.FC = () => {
+    return (
+        <div className="App">
+            <MainBreadcrumbs />{' '}
+        </div>
+    );
+};
+
+export default TrendsPage;
